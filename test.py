7ee9899659b3c1728e6c8cdd0f0a76b6c3b1f72b
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-05-29 15:54:37 +0200

    Updated test

--- test.py
+++ test.py
@@ -2,7 +2,9 @@
 from json import load
 from os import walk
 
-from dictances import normal_total_variation
+from dictances import (bhattacharyya, hellinger, jensen_shannon,
+                       kullback_leibler, normal_total_variation)
+from tqdm import tqdm
 
 from zipf_classifier import ZipfBinaryClassifier
 
@@ -13,11 +15,15 @@ def get_options(root):
         return load(f)
 
 
-test_root = "/Users/lucacappelletti/Datasets/parsed_zipfs/zipfs"
+test_root = "/Users/lucacappelletti/Datasets/zipf_datasets/zipfs"
 dataset_root = "/Users/lucacappelletti/Datasets/zipf_datasets/for_datasets"
 test_zipfs = [x[0] for x in walk(test_root) if x[0] != test_root]
 
-for current_test in test_zipfs[:1]:
+metrics = [bhattacharyya, hellinger, jensen_shannon,
+           kullback_leibler, normal_total_variation]
+
+for i, current_test in enumerate(test_zipfs):
+    print("\033[1mRunning test %s of %s\033[0;0m" % (i + 1, len(test_zipfs)))
     classifier = ZipfBinaryClassifier(get_options(current_test))
     print("Current classifier has options: %s\n" % classifier)
 
@@ -40,14 +46,32 @@ for current_test in test_zipfs[:1]:
         ]
     }
 
-    for expected, paths in zipfs.items():
-        for path in paths:
-            classifier.add_zipf("%s/%s.json" % (current_test, path), expected)
+    z_path = current_test + "/%s.json"
+
+    [classifier.add_zipf(z_path % path, expected) for expected, paths in tqdm(
+        zipfs.items(),
+        desc="Loading zipfs",
+        unit=' class',
+        leave=True,
+        unit_scale=True
+    ) for path in tqdm(
+        paths,
+        desc="Loading zipfs of class %s" % expected,
+        unit=' zipf',
+        leave=True,
+        unit_scale=True
+    )]
+
+    print("\n")
 
     classifier.render_baseline()
 
+    print("\n")
+
     for expected, paths in datasets.items():
         for path in paths:
-            print("\033[1mWorking on %s \033[0;0m" % path)
-            classifier.run("%s/%s" % (dataset_root, path),
-                           expected, normal_total_variation)
+            print("\033[1mWorking on %s \033[0;0m\n" % path)
+            for metric in metrics:
+                print("\033[1mUsing metric %s \033[0;0m\n" % metric.__name__)
+                classifier.run("%s/%s" % (dataset_root, path),
+                               expected, normal_total_variation)
